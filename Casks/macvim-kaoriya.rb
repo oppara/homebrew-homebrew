4ee7eb7a7e6610e708bce3af7dddb0d9ab2318eb
oppara <oppara@oppara.tv>
2014-05-03 23:46:23 +0900

    Add cask macvim-kaoriya

---
+++ Casks/macvim-kaoriya.rb
@@ -0,0 +1,7 @@
+class MacvimKaoriya < Cask
+  url 'https://github.com/splhack/macvim/releases/download/20140501/macvim-kaoriya-20140501.dmg'
+  sha256 'f45deb0f7dc399b248ff857b339542d6a3bdfee5a49bbad28ec09ad9b105bebc'
+  homepage 'https://code.google.com/p/macvim-kaoriya/'
+  version '20140501'
+  link 'MacVim.app'
+end

68acfe97c7d80b52d35d81fd2a1d92b835976ff1
oppara <oppara@oppara.tv>
2014-05-03 23:29:32 +0900

    Add cask mi-beta

---
+++ Casks/mi-beta.rb
@@ -0,0 +1,7 @@
+class MiBeta < Cask
+  url 'http://www.mimikaki.net/download/mi3.0.0b5.dmg'
+  homepage 'http://www.mimikaki.net/'
+  version '3.0.0b5'
+  sha256 '57451ce6a78d164ff12751b8342407c2acc741f7034a15b3d226111a6841bb38'
+  link 'mi.app'
+end

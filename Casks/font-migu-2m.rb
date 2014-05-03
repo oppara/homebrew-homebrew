c337571cf83818ef720a8431d58408ecf2171dc8
oppara <oppara@oppara.tv>
2014-05-04 00:52:45 +0900

    Add cask font-migu-2m

---
+++ Casks/font-migu-2m.rb
@@ -0,0 +1,8 @@
+class FontMigu2m < Cask
+  url 'http://sourceforge.jp/frs/redir.php?m=osdn&f=%2Fmix-mplus-ipa%2F59022%2Fmigu-2m-20130617.zip'
+  homepage 'http://mix-mplus-ipa.sourceforge.jp/migu/'
+  version '20130617'
+  sha256 '25092541e927bbebc094f9c1d7cfaec3e8e2c7a997c360c507a3433ed11e8278'
+  font 'migu-2m-20130617/migu-2m-regular.ttf'
+  font 'migu-2m-20130617/migu-2m-bold.ttf'
+end

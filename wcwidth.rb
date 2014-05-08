c282827af4f75c533b334a4b63780f233920ddb5
oppara <oppara@oppara.tv>
2014-05-08 23:31:54 +0900

    Update wcwidth caveats

--- wcwidth.rb
+++ wcwidth.rb
@@ -11,4 +11,24 @@ class Wcwidth < Formula
     bin.install_symlink "#{prefix}/usr/local/bin/wcwidth-cjk"
     lib.install_symlink "#{prefix}/usr/local/lib/wcwidth-cjk.so"
   end
+
+  def caveats; <<-EOS.undent
+    Run a command with $LD_PRELOAD-able library:
+
+        $ export LD_PRELOAD=/usr/local/lib/wcwidth-cjk.so
+        $ zsh
+        ...
+
+    or:
+
+        $ eval `/usr/local/bin/wcwidth-cjk --sh-init`
+        $ zsh
+        ...
+
+    Run a command via wrapper script:
+
+        $ /usr/local/bin/wcwidth-cjk zsh
+        ...
+    EOS
+  end
 end

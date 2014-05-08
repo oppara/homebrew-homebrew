05e39c0ea1e5e603078dff4cb7788ee5272b9e2b
oppara <oppara@oppara.tv>
2014-05-09 01:15:32 +0900

    Add QuicklookJapaneseText

---
+++ Casks/quicklook-japanese-text.rb
@@ -0,0 +1,7 @@
+class QuicklookJapaneseText < Cask
+  url 'http://x.nest.jp/attachment/JapaneseText_qlgenerator-1.0.zip'
+  homepage 'https://github.com/ento/quicklook-jptxt'
+  version '1.0'
+  sha256 'c2e03b0fb827abdbc69f7523e9ea1296928b7c1427bf56e5d900a4b7e2c31684'
+  qlplugin 'JapaneseText.qlgenerator'
+end

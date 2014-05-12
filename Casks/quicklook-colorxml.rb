ee3f0ea69a4bb670edb06499a3ca73fbbaf1f268
oppara <oppara@oppara.tv>
2014-05-13 02:15:04 +0900

    Add QuicklookColorxml

---
+++ Casks/quicklook-colorxml.rb
@@ -0,0 +1,7 @@
+class QuicklookColorxml < Cask
+  url 'http://www.araneum.it/macosx/colorxml-QuickLook-1.1.zip'
+  homepage 'http://www.araneum.it/macosx/'
+  version '1.1'
+  sha256 '0483060fe0ff5a0d159e280a7da55135c5c2823dc59ae4fdd1fd94ae11057985'
+  qlplugin 'colorxml.qlgenerator'
+end

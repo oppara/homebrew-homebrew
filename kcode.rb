da3e18c8da6c51a7bd9c1a9c259e6eddefd09ef2
oppara <oppara@oppara.tv>
2016-08-04 23:23:21 +0900

    Update kcode version

--- kcode.rb
+++ kcode.rb
@@ -4,6 +4,7 @@ class Kcode < Formula
   homepage 'http://www.irori.org/tool/kcode.html'
   url 'http://www.irori.org/dl/tool/kcode-1.0.gz'
   shq256 '477c85ad8cd4cdfd310c3110a16b3bba1d88b803e4c19d661d794ee13af2a632'
+  version '1.0.1'
 
   def install
     File.rename 'kcode-1.0', 'kcode'

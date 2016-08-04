39490dedc6c4767919477f7f74314e83eac26995
oppara <oppara@oppara.tv>
2016-08-04 23:23:03 +0900

    Use Formula.sha256 kcode

--- kcode.rb
+++ kcode.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Kcode < Formula
   homepage 'http://www.irori.org/tool/kcode.html'
   url 'http://www.irori.org/dl/tool/kcode-1.0.gz'
-  sha1 '61745eff33cb8c962d91eeeb707c639910c7a27b'
+  shq256 '477c85ad8cd4cdfd310c3110a16b3bba1d88b803e4c19d661d794ee13af2a632'
 
   def install
     File.rename 'kcode-1.0', 'kcode'

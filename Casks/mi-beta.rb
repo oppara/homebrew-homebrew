d6512bf4d330273ca266c6cdd2370cef4f31c3c0
oppara <oppara@oppara.tv>
2018-03-05 23:46:44 +0900

    Update mi 3.0.0b16

--- Casks/mi-beta.rb
+++ Casks/mi-beta.rb
@@ -1,6 +1,6 @@
 cask 'mi-beta' do
-  url 'https://www.mimikaki.net/download/mi3.0.0b14.dmg'
+  url 'https://www.mimikaki.net/download/mi3.0.0b16.dmg'
   homepage 'http://www.mimikaki.net/'
-  version '3.0.0b14'
-  sha256 '149c76f64963e72e569f7acf862690cf6ace8169eadd0261392d0462422161b4'
+  version '3.0.0b16'
+  sha256 '49e239009e70edc2dc5751f9b6b7216b9584f09fe4df2be88991b58468418c38'
 end

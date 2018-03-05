33e02e3cd7eeab29ea09393deb642dd769286359
oppara <oppara@oppara.tv>
2018-03-05 23:52:06 +0900

    Remove mi-beta
    available caskroom/versions/mi-beta

--- Casks/mi-beta.rb
+++
@@ -1,6 +0,0 @@
-cask 'mi-beta' do
-  url 'https://www.mimikaki.net/download/mi3.0.0b16.dmg'
-  homepage 'http://www.mimikaki.net/'
-  version '3.0.0b16'
-  sha256 '49e239009e70edc2dc5751f9b6b7216b9584f09fe4df2be88991b58468418c38'
-end

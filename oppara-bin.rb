eb580e7f440ec244b4ca21ee83117167f7f56de8
oppara <oppara@oppara.tv>
2021-10-20 21:33:55 +0900

    :bug: (brew) Remove deprecated bottle :unneeded
    Warning: Calling bottle :unneeded is deprecated! There is no replacement.

--- oppara-bin.rb
+++ oppara-bin.rb
@@ -2,8 +2,6 @@ class OpparaBin < Formula
   homepage 'https://github.com/oppara/bin'
   head 'https://github.com/oppara/bin', using: :git
 
-  bottle :unneeded
-
   def install
     bin.install Dir["*"]
   end

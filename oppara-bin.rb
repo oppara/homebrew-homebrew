1bb08bc9144dfbd50aa3bc693dc19039b67fd1c2
oppara <oppara@oppara.tv>
2025-01-26 10:27:29 +0900

    feat(bin): version 0.0.2

--- oppara-bin.rb
+++ oppara-bin.rb
@@ -1,6 +1,7 @@
 class OpparaBin < Formula
   homepage 'https://github.com/oppara/bin'
   head 'https://github.com/oppara/bin', using: :git
+  version '0.0.2'
 
   def install
     bin.install Dir["*"]
